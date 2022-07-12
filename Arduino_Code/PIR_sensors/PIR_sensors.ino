75bc74d33572c0a63e3a6b56608f12b154d68a85
Clément Bourguignon <clement.bourguignon@mail.mcgill.ca>
2022-07-12 18:28:48 -0400

    Update PIR_sensors.ino
    corrected a typo ',' > ';' in loop

--- Arduino_Code/PIR_sensors/PIR_sensors.ino
+++ Arduino_Code/PIR_sensors/PIR_sensors.ino
@@ -1,6 +1,6 @@
 // change number of PIR sensors and first pin here:
 const int n_pirs = 12;
-const int first_pin = 12;
+const int first_pin = 0;
 
 int PIR_chan[n_pirs];
 int PIR[n_pirs];
@@ -8,7 +8,7 @@ int PIR[n_pirs];
 void setup() {
   Serial.begin(115200);
   // Set all channels to pullup inputs
-  for (int i = 0; i < n_pirs, i++) {
+  for (int i = 0; i < n_pirs; i++) {
     PIR_chan[i] = i + first_pin;
     pinmode(PIR_chan[i], INPUT_PULLUP);
   }
@@ -16,12 +16,12 @@ void setup() {
 
 void loop() {
   // Print n-1 first chans with tab
-  for (int i = 0; i < n_pirs-1, i++) {
+  for (int i = 0; i < n_pirs-1; i++) {
     Serial.print(String(digitalRead(PIR_chan[i])) + '\t');
   }
   // Print last chan with carriage return
   Serial.print(String(digitalRead(PIR_chan[n_pirs-1])) + '\n');
 
-  // 4 Hz is enough
+  // set sampling period, 4 Hz is more than enough
   delay(250);
 }
